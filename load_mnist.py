3cdf4ca8c5c5e5ba905c8d4b2cf93305620b9209
LongPhanPip <phanlonghd5200@gmail.com>
2021-05-30 09:34:18 +0700

    Update load_data

--- load_mnist.py
+++ load_mnist.py
@@ -2,14 +2,16 @@ from mnist import MNIST
 import random
 import numpy as np
 import os
+import cv2
 
 MNIST_DIR = 'minist_data'
 TRAIN_DIR = os.path.join(MNIST_DIR, 'train')
 TEST_DIR = os.path.join(MNIST_DIR, 'test')
 
-mndata = MNIST(TRAIN_DIR)
 
-images, labels = mndata.load_training()
-
-index = random.randrange(0, len(images))
-print(mndata.display(images[index]))
+def load_data():
+    mndata = MNIST(TRAIN_DIR)
+    X_train, Y_train = mndata.load_training()
+    mndata = MNIST(TEST_DIR)
+    X_test, Y_test = mndata.load_testing()
+    return np.array(X_train), np.array(Y_train), np.array(X_test), np.array(Y_test)
